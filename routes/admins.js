f13e59ea127f66544ce8a11211c31aa1cb0855a3
Ahmzyjazzy <olanrewajuahmed095@yahoo.com>
2019-05-20 19:20:44 +0100

    feat/update api

--- routes/admins.js
+++ routes/admins.js
@@ -42,61 +42,41 @@ router.post('/authenticate', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    if (username == 'admin2' && password == 'password2') {
-        const token = jwt.sign({ data: username }, config.secret, {
-            expiresIn: 604800 // token expires in one week
-        });
+    AdminUser.getAdminUserByUsername(username, (err, adminUser) => {
+        if (err) throw err;
 
-        res.json({
-            success: true,
-            token: 'jwt4admin ' + token
-            // adminUser: {
-            //     id: adminUser._id,
-            //     firstname: adminUser.firstname,
-            //     lastname: adminUser.lastname,
-            //     email: adminUser.email,
-            //     mobile_no: adminUser.mobile_no,
-            //     username: adminUser.username
-            // }
+        if (!adminUser) {
+            return res.json({ success: false, msg: "User not Found" });
+        }
+
+        //this can be done in ur model FYI
+        AdminUser.comparePassword(password, adminUser.password, (err, isMatch) => {
+            if (err) throw err;
+
+            if (isMatch) {
+                const token = jwt.sign({ data: adminUser }, config.secret, {
+                    expiresIn: 604800 // token expires in one week
+                });
+
+                res.json({
+                    success: true,
+                    token: 'jwt4admin ' + token
+                    // adminUser: {
+                    //     id: adminUser._id,
+                    //     firstname: adminUser.firstname,
+                    //     lastname: adminUser.lastname,
+                    //     email: adminUser.email,
+                    //     mobile_no: adminUser.mobile_no,
+                    //     username: adminUser.username
+                    // }
+                })
+            } else {
+                return res.json({ success: false, msg: 'Wrong password' });
+            }
         })
-    } else {
-        return res.json({ success: false, msg: 'Wrong password' });
-    }
 
-    // AdminUser.getAdminUserByUsername(username, (err, adminUser) => {
-    //     if (err) throw err;
-
-    //     if (!adminUser) {
-    //         return res.json({ success: false, msg: "User not Found" });
-    //     }
-
-    //     //this can be done in ur model FYI
-    //     AdminUser.comparePassword(password, adminUser.password, (err, isMatch) => {
-    //         if (err) throw err;
-
-    //         if (isMatch) {
-    //             const token = jwt.sign({ data: adminUser }, config.secret, {
-    //                 expiresIn: 604800 // token expires in one week
-    //             });
-
-    //             res.json({
-    //                 success: true,
-    //                 token: 'jwt4admin ' + token
-    //                 // adminUser: {
-    //                 //     id: adminUser._id,
-    //                 //     firstname: adminUser.firstname,
-    //                 //     lastname: adminUser.lastname,
-    //                 //     email: adminUser.email,
-    //                 //     mobile_no: adminUser.mobile_no,
-    //                 //     username: adminUser.username
-    //                 // }
-    //             })
-    //         } else {
-    //             return res.json({ success: false, msg: 'Wrong password' });
-    //         }
-    //     })
-
-    // })
+    })
+    
 });
 
 module.exports = router;
