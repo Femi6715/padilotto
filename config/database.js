a92d01114ab97cf8bd819684d213152adf3302f8
Femi6715 <124842466+Femi6715@users.noreply.github.com>
2025-03-27 07:55:21 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,13 @@
-module.exports = {
-    database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/?retryWrites=true&w=majority&appName=padi",
-    secret: "whatismysecret"
-}
+// module.exports = {
+//     database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/?retryWrites=true&w=majority&appName=padi",
+//     secret: "whatismysecret"
+// }
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/?retryWrites=true&w=majority&appName=padi';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = mongoose;
