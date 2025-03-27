45db6555bf7e1f4356baf772e5d385ad04b0c25c
Femi6715 <124842466+Femi6715@users.noreply.github.com>
2025-03-27 16:25:38 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,4 @@
-// config/database.js
-const mongoose = require('mongoose');
-
-// Ensure the fallback string includes the database name ("padi") and all query parameters:
-const mongoURI = process.env.MONGO_URI || 
-  'mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true&w=majority&appName=padi';
-
-console.log('Using mongoURI:', mongoURI); // Debug: Check what is being used
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
-module.exports = mongoose;
+module.exports = {
+    database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true",
+    secret: "yourSuperSecretKey"
+}
