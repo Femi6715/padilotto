d0f3be757bae1cdf283ea384ffdaa05d92edaca2
Femi6715 <124842466+Femi6715@users.noreply.github.com>
2025-03-27 16:10:12 -0400

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,22 +1,19 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models/user');
-const config = require('../config/database');
-const passport = require('passport');
+const { config } = require('./database'); // Or wherever you store your secret
+const AdminUser = require('../models/admin');
 
 let opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
-opts.secretOrKey = config.secret;
-passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    User.getUserById(jwt_payload.data._id, (err, user) => {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-        }
-    });
-}));
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+// Use an environment variable if available; otherwise, fallback to config.secret
+opts.secretOrKey = process.env.JWT_SECRET || config.secret;
 
+module.exports = passport => {
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    AdminUser.findById(jwt_payload.data._id, (err, user) => {
+      if (err) return done(err, false);
+      if (user) return done(null, user);
+      return done(null, false);
+    });
+  }));
+};
