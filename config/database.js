e74381993499bb58e9da602bbf7ffce4676eea92
Femi6715 <124842466+Femi6715@users.noreply.github.com>
2025-03-27 16:31:59 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true",
+    database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/?retryWrites=true&w=majority&appName=padi",
     secret: "yourSuperSecretKey"
 }
