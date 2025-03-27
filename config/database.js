6fc925de4070fcff1b111acc7b1285945ee4efb7
Femi6715 <124842466+Femi6715@users.noreply.github.com>
2025-03-27 19:28:31 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// module.exports = {
-//     database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true",
-//     secret: "yourSuperSecretKey"
-// }
+module.exports = {
+    database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true",
+    secret: "yourSuperSecretKey"
+}
 
 
-// // mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-// //   .then(() => console.log('MongoDB connected'))
-// //   .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // // // Option 1: Export only mongoose
 // // //module.exports = mongoose;
@@ -17,10 +17,10 @@
 // // module.exports = { mongoose, secret: 'yourSuperSecretKey' };
 
 // config/database.js
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// Use the environment variable MONGO_URI if set, otherwise use the fallback string.
-const mongoURI = process.env.MONGO_URI || 'mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true&w=majority&appName=padi';
+// // Use the environment variable MONGO_URI if set, otherwise use the fallback string.
+// const mongoURI = process.env.MONGO_URI || 'mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true&w=majority&appName=padi';
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
