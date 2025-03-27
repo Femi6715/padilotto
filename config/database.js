6fa2f4c3f759b36cbe7ef8a2b46bac127cbc24ac
Femi6715 <124842466+Femi6715@users.noreply.github.com>
2025-03-27 16:46:16 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,13 @@
-module.exports = {
-    database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/?retryWrites=true&w=majority&appName=padi",
-    secret: "yourSuperSecretKey"
-}
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true&w=majority&appName=padi';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Option 1: Export only mongoose
+//module.exports = mongoose;
+
+// Option 2: Export an object containing mongoose and other config values
+module.exports = { mongoose, secret: 'yourSuperSecretKey' };
