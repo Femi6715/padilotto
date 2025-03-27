907962f5ab890a9e1c3517335d2e49c99ca98664
Femi6715 <124842466+Femi6715@users.noreply.github.com>
2025-03-27 19:23:24 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,17 +1,34 @@
+// const mongoose = require('mongoose');
+
+// module.exports = {
+//     database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true",
+//     secret: "yourSuperSecretKey"
+// }
+
+
+// // mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+// //   .then(() => console.log('MongoDB connected'))
+// //   .catch(err => console.error('MongoDB connection error:', err));
+
+// // // Option 1: Export only mongoose
+// // //module.exports = mongoose;
+
+// // // Option 2: Export an object containing mongoose and other config values
+// // module.exports = { mongoose, secret: 'yourSuperSecretKey' };
+
+// config/database.js
 const mongoose = require('mongoose');
 
-module.exports = {
-    database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true",
-    secret: "yourSuperSecretKey"
-}
+// Use the environment variable MONGO_URI if set, otherwise use the fallback string.
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true&w=majority&appName=padi';
 
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // For testing only (not recommended in production):
+  tlsAllowInvalidCertificates: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
-// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error('MongoDB connection error:', err));
-
-// // Option 1: Export only mongoose
-// //module.exports = mongoose;
-
-// // Option 2: Export an object containing mongoose and other config values
-// module.exports = { mongoose, secret: 'yourSuperSecretKey' };
+module.exports = mongoose;
