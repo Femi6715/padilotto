65dc638f3e02f0857e715e4eabb31f2796d0c3a5
Femi6715 <124842466+Femi6715@users.noreply.github.com>
2025-04-06 20:24:09 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,4 @@
-const mongoose = require('mongoose');
-
 module.exports = {
     database: "mongodb+srv://admin:62221085@padi.rfdah5x.mongodb.net/padi?retryWrites=true",
     secret: "yourSuperSecretKey"
 }
-
-
-
